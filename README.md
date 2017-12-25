b37452ca2fb1b73da13971a72b2872bece909eaa
xiongzhenggang <ab1290014555@qq.com>
2017-12-25 21:29:43 +0800

    spring bootcache

---
+++ README.md
@@ -0,0 +1,7 @@
+## spring boot 简化了很多spring操作，但是有时候除了问题也会让人一头雾水。本项目单纯对springboot
+## 学习以及会对spring boot缺少的配置做相应的提示
+###  springboot 启用缓存非常的简单
+*  spring 使用缓存基本原理是使用aop 拦截的原理，加上相关的注解方便用户实使用，下面就springboot和spring的缓存配置做简要的介绍
+```
+
+```
--- src/main/java/com/xzg/cn/configure/RedisCacheConfig.java
+++ src/main/java/com/xzg/cn/configure/RedisCacheConfig.java
@@ -1,9 +1,18 @@
-package com.xzg.cn.configure;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching//启动
-public class RedisCacheConfig {
-}
+//package com.xzg.cn.configure;
+//
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//@Configuration
+//@EnableCaching//启动
+//public class RedisCacheConfig {
+//
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate redisTemplate){
+//        return new RedisCacheManager(redisTemplate);
+//    }
+//}
