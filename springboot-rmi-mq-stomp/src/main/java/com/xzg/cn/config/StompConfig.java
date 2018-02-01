af825afb06be2ee817dd0783e6a4119d2f9a9d55
xiongzhenggang <ab1290014555@qq.com>
2018-02-01 22:14:18 +0800

    rabbitmq

--- springboot-rmi-mq-stomp/src/main/java/com/xzg/cn/config/StompConfig.java
+++ springboot-rmi-mq-stomp/src/main/java/com/xzg/cn/config/StompConfig.java
@@ -21,8 +21,7 @@ public class StompConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         // 允许使用socketJs方式访问，访问点为webSocketServer，允许跨域
         // 在网页上我们就可以通过这个链接
-        // http://localhost:8080/webSocketServer
-        // 来和服务器的WebSocket连接
+        // http://localhost:8080/webSocketServer来和服务器的WebSocket连接
         stompEndpointRegistry.addEndpoint("/webSocketServer").setAllowedOrigins("*").withSockJS();
     }
     /**
@@ -32,16 +31,22 @@ public class StompConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 订阅Broker名称
         //enableSimpleBroker 是spring提供简单的代理，基于内存实现，如果需要更高级功能可以选择rabbit、activitie mq
-        registry.enableSimpleBroker("/queue", "/topic");
-        //启用stomp代理中继
-        /*registry.enableStompBrokerRelay("/topic","/queue")
-                .setRelayHost("rabbit.somewhere")
-                .setRelayPort(62321)
+//        registry.enableSimpleBroker("/queue", "/topic");
+        //启用stomp代理中继 启动Rabbit stomp插件： rabbitmq-plugins enable rabbitmq_stomp
+        registry.enableStompBrokerRelay("/topic","/queue")
+                .setRelayHost("192.165.1.105")
+                //amqp:5672 clustering:5672  http:15672
+                .setRelayPort(61613)
                 .setClientLogin("guest")
-                .setClientPasscode("guest");*/
+                .setClientPasscode("guest")
+                .setSystemLogin("guest")
+                .setSystemPasscode("guest")
+                .setSystemHeartbeatSendInterval(5000)
+                .setSystemHeartbeatReceiveInterval(4000);
         // 全局使用的消息前缀（客户端订阅路径上会体现出来）
         registry.setApplicationDestinationPrefixes("/app");
         // 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/
          registry.setUserDestinationPrefix("/user/");
     }
 }
+
